5b437eb0569380e5e0498721d51c012d90df8ce0
Eslam <eslambadawy25688@gmail.com>
2025-07-10 12:15:54 +0300

    message

--- app/Http/Controllers/Api/OfferController.php
+++ app/Http/Controllers/Api/OfferController.php
@@ -21,7 +21,7 @@ class OfferController extends Controller
         if (!$main_offer) {
             return response()->apiFail('Failed to add Offer', 500);
         }
-        return response()->apiSuccess( 'Offer added successfully' , $main_offer );
+        return response()->apiSuccess(  $main_offer , 'Offer added successfully'  );
     }
     public function update(Request $request, $id)
     {
@@ -29,7 +29,7 @@ class OfferController extends Controller
         if (!$offer) {
             return response()->apiFail('Offer not found', 404);
         }
-        return response()->apiSuccess('Offer updated successfully' ,$offer );
+        return response()->apiSuccess($offer,'Offer updated successfully' );
     }
     public function get_offer($id)
     {
@@ -52,6 +52,6 @@ class OfferController extends Controller
         if (!$offer) {
             return response()->apiFail('Offer not found', 404);
         }
-        return response()->apiSuccess( 'Offer deleted successfully',$offer );
+        return response()->apiSuccess( 'Offer deleted successfully'  );
     }
 }
